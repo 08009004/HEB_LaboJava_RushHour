fb49a0c3e787468b2c36a5318e96c1467f470b53
g42116 <g42116@L301P03.pdg.esi.heb.site>
2016-03-07 09:46:08 +0100

    initial commit

---
+++ src/g42116/rushhour/model/Direction.java
@@ -0,0 +1,11 @@
+package g42116.rushhour.model;
+
+/**
+ * This class is an enumeration of all the directions it is possible to move a
+ * car.
+ * @author g42116
+ */
+public enum Direction {
+    LEFT
+    
+}

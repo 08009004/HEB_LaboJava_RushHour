112dbbc77be3b8c9c2ff80f56eaf58d61ba96d6e
john <john@john-VGN-NW20SF-S>
2016-04-17 00:40:59 +0200

    fixed: Json Library v1.0 -> v1.0.2 to do: implement Json language object usage

--- src/g42116/rushhour/view/tempTest.java
+++ src/g42116/rushhour/view/tempTest.java
@@ -6,6 +6,8 @@
 package g42116.rushhour.view;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.StringReader;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
@@ -19,19 +21,22 @@ import javax.json.JsonReader;
  * @author john
  */
 public class tempTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 //        String s = File.separator;
 //        File langPath = new File("blablabla");
 //        Path path = FileSystems.getDefault().getPath("logs", "TextsEnglish");
 
-        
+//FileReader is = new FileReader("/home/NetBeansProjects/RushHour-Ruiz/src/g42116/rushhour/view/TextsEnglish.json");
+FileReader is = new FileReader("src/g42116/rushhour/view/TextsEnglish.json");
+JsonReader rdr = Json.createReader(is);
+rdr.close();
         
         
 // The class Json contains methods to create readers from input sources (InputStream and Reader)
 // The following example demonstrates how to read an empty JSON array from a string: 
-        JsonReader jsonReader = Json.createReader(new StringReader("[]"));
-        JsonArray array = jsonReader.readArray();
-        jsonReader.close();
+//      JsonReader jsonReader = Json.createReader(new StringReader("[]"));
+//        JsonArray array = jsonReader.readArray();
+//        jsonReader.close();
         
         
         

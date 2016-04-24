83f401313bea3793ebcd5a95d5cbbae3d5855da0
john <john@john-VGN-NW20SF-S>
2016-04-24 20:45:16 +0200

    created more json initial board files

--- src/g42116/rushhour/RushHour.java
+++ src/g42116/rushhour/RushHour.java
@@ -5,7 +5,7 @@ import g42116.rushhour.model.Position;
 import g42116.rushhour.model.RushHourException;
 import g42116.rushhour.model.RushHourGame;
 import g42116.rushhour.view.RushHourView;
-import static g42116.rushhour.view.Display.*;
+import static g42116.rushhour.view.Display.displayBoard;
 import java.io.File;
 import java.util.List;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -24,62 +24,56 @@ public class RushHour {
      * @param args unused
      */
     public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
-        File gameInitFile = new File("src/g42116/rushhour/games/GameTest3.json");
+        File initialBoard = new File("src/g42116/rushhour/games/Game3.json");
         RushHourGame game = null;
-        JsonNode initNode;
-        int boardHeight;
-        int boardWidth;
         try {
-            initNode = mapper.readValue(gameInitFile, JsonNode.class);           
-            boardHeight = initNode.path("boardHeight").asInt();
-            boardWidth = initNode.path("boardWidth").asInt();
-            Position exit = mapper.readValue(initNode.path("exit").toString(), Position.class);
-            Car redCar = mapper.readValue(initNode.path("redCar").toString(), Car.class);
-
-            List<Car> otherCars = mapper.readValue(initNode.path("otherCars").toString(), 
-                mapper.getTypeFactory().constructCollectionType(List.class, Car.class));
-            System.out.println("6");
-            game = new RushHourGame(boardHeight, boardWidth, exit, redCar, otherCars);
-        } catch (IOException ioe) {
+            game = initGame(initialBoard);
+        } catch (IOException | RushHourException ioe) {
             System.out.println("Impossible to start the game. " 
                     + ioe.getMessage().replace(
-                            "g42116.rushhour.model.RushHourException: ", ""));
-        } catch (RushHourException rhe) {
-            System.out.println("Impossible to start the game. " 
-                    + rhe.getMessage().replace(
-                            "g42116.rushhour.model.RushHourException: ", ""));
+                              "g42116.rushhour.model.RushHourException: ", ""));
         }
-
-        
         RushHourView view = new RushHourView(game);
 
         displayBoard(game.getBoard());
         view.play();
-        
-/*        Position exit = new Position(2, 5);
-        Car redCar = new Car('R', 2, HORIZONTAL, new Position(2,0));
-        List<Car> otherCars = Arrays.asList(
-            new Car('1', 3, HORIZONTAL, new Position(1,2)),
-            new Car('2', 2, VERTICAL, new Position(2,3)),
-            new Car('3', 4, HORIZONTAL, new Position(5,0))
-        );
+    }
 
-        RushHourGame game;
+    /**
+     * Initialises the game object with parameters recovered from a Json file.
+     * 
+     * @param   initialBoard    the Json file
+     * @return                  the game
+     * @throws                  java.io.IOException if 'initialBoard' refers to
+     *                          an invalid file or filepath.
+     * @throws                  g42116.rushhour.model.RushHourException if the
+     *                          game arguments are incorrect or illegal.
+     */
+    public static RushHourGame initGame(File initialBoard) 
+                                         throws IOException, RushHourException {
 
-        try {
-            game = new RushHourGame(6, 6, exit, redCar, otherCars);
-            RushHourView view = new RushHourView(game);
-            clearScreen();
-            displayBoard(game.getBoard());
-            view.play();
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readValue(initialBoard, JsonNode.class);
 
-        } catch (RushHourException rhe) {
-            System.out.println("Impossible to start the game. " 
-                    + rhe.getMessage().replace(
-                            "g42116.rushhour.model.RushHourException: ", ""));
-        }
+        int boardHeight = rootNode.path("boardHeight").asInt();
+        int boardWidth = rootNode.path("boardWidth").asInt();
+
+        Position exit = mapper.readValue(
+                              rootNode.path("exit").toString(), Position.class);
+        Car redCar = mapper.readValue(
+                                 rootNode.path("redCar").toString(), Car.class);
+
+/* NOTES:
+http://www.leveluplunch.com/java/examples/convert-json-array-to-arraylist-of-objects-jackson/
+https://github.com/FasterXML/jackson-databind
 */
+        List<Car> otherCars = mapper.readValue(
+                rootNode.path("otherCars").toString(), 
+                        mapper.getTypeFactory().constructCollectionType(
+                                                        List.class, Car.class));
+
+        return new RushHourGame(
+                              boardHeight, boardWidth, exit, redCar, otherCars);
     }
 
 }
